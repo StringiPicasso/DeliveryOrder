bb390513ce097d71831ef4d53fc994725fe81994
Nastya <nastya.eremina.98@gmail.com>
2023-12-02 00:26:45 +0300

    CreateREadMe

---
+++ DeliveryOrder/DeliveryOrder/Migrations/20231201210549_DeleteSeler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryOrder.Migrations
+{
+    public partial class DeleteSeler : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sender",
+                table: "Deliveries");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Sender",
+                table: "Deliveries",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
